5b8c46b23cd3122ac1b783a446dce392c17f2fd4
antnf3 <antnf3@gmail.com>
2019-02-24 12:17:18 +0900

    Updates

---
+++ precache-manifest.6de546df12776f0d7838fc063bbfd5fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3de7d8459a3fcd338508",
+    "url": "/privacy_ostar/static/js/runtime~main.3de7d845.js"
+  },
+  {
+    "revision": "373ec914a2313e0c5c69",
+    "url": "/privacy_ostar/static/js/main.373ec914.chunk.js"
+  },
+  {
+    "revision": "c88c70e5f4ff8bea6fac",
+    "url": "/privacy_ostar/static/js/2.c88c70e5.chunk.js"
+  },
+  {
+    "revision": "373ec914a2313e0c5c69",
+    "url": "/privacy_ostar/static/css/main.be7cd5a8.chunk.css"
+  },
+  {
+    "revision": "ca90fcdb63af413c412f299683aa312f",
+    "url": "/privacy_ostar/index.html"
+  }
+];
